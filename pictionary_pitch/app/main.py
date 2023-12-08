65d521f9797947322580da7e9bfe26b53b10d2e5
blue-Cloud32 <cau45glose@gmail.com>
2023-12-08 14:27:23 -0700

    added socketio dependenices

--- pictionary_pitch/app/main.py
+++ pictionary_pitch/app/main.py
@@ -1,15 +1,16 @@
 from flask import Flask, render_template
 from flask import request
-import socket
+from flask_socketio import SocketIO
 
 app = Flask(__name__)
+socketio = SocketIO(app)
 
 @app.route('/')
 def index():
 
-
-
     return render_template('index.html')
+
+
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5555))
