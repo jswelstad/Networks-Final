7f98aafdd0fd2a005053a628cf870942ae1aa003
kcverbrugge <kverbruggestudy@gmail.com>
2023-12-12 18:21:24 -0700

    More changes to the Game and Player class

--- kyle.py
+++ kyle.py
@@ -18,36 +18,40 @@ def startTimer():
         continue 
 
 class Player:
+    def __init__(self, name, socket, points):
+        self.username = name
+        self.socket = socket
+        self.points = 0
 
 class Game: #my vision is that the Game class will be used on the server side to handle game logic.
     def __init__(self, roundTime, maxPlayers, maxRounds, fileName):
-        self.maxGameTime = roundTime
+        self.maxRoundTime = roundTime
         self.maxPlayerCount = maxPlayers
         self.maxRounds = maxRounds
         self.roundCount = 0
-        self.startingTime
-        self.gameFinished =  False
+        self.timer 
+        self.gameFinished = False
         self.gameStarted = False
-        self.playerSet = []
-        self.wordSet = []
-        loadWordSet(fileName)
+        self.playerList = []
+        self.wordList = []
+        loadwordList(fileName)
 
-    def addPlayer(self, playerName):
-        self.playerSet.insert(playerName)
+    def addPlayer(self, player):
+        self.playerList.insert(player)
 
     def deletePlayer(self, playerName):
-        if (len(self.playerSet) < 1):
+        if (len(self.playerList) < 1):
             print("No players to delete")
-        elif playerName in self.playerSet:
-            self.playerSet.remove(playerName)
-            if (len(self.playerSet) <= 1 and self.gameStarted == True):\
+        elif playerName in self.playerList:
+            self.playerList.remove(playerName)
+            if (len(self.playerList) <= 1 and self.gameStarted == True):
                 print("Not enough players to continue the game.")
                 self.gameFinished == True
         else:
             print(f"{playerName} not found in database.")
     
     def startGame(self):
-        if (len(self.playerSet) <= 1 or self.gameStarted == True):
+        if (len(self.playerList) <= 1 or self.gameStarted == True):
             print("Unable to start game because either there aren't at least 2 players or there is already a game going.")
             return
         else:
@@ -58,11 +62,11 @@ class Game: #my vision is that the Game class will be used on the server side to
             self.gameFinished = True
             return
         self.roundCount += 1
-        self.startingTime = time.time()
+        self.timer = time.time()
 
     def getTimeRemaining(self):
         if (self.gameStarted):
-            return time.time() - self.startingTime
+            return time.time() - self.timer
         else:
             print("Round has not started")
             return -1
@@ -73,11 +77,11 @@ class Game: #my vision is that the Game class will be used on the server side to
         self.gameStarted = False
 
     def randomWordGenerator(self):
-        randomNumber = random.randrange(0, len(self.wordSet))
-        return self.wordSet[randomNumber]
+        randomNumber = random.randrange(0, len(self.wordList))
+        return self.wordList[randomNumber]
     
-    def loadWordSet(self, fileName):
+    def loadwordList(self, fileName):
         with open(fileName, 'r') as file:
-            self.wordSet = file.readlines()
+            self.wordList = file.readlines()
         
     
